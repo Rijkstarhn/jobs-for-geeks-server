d2276ff3b0a706a8036d5ee912a96f1b1daec328
Rijkstarhn <32129291+Rijkstarhn@users.noreply.github.com>
2021-04-17 16:48:26 -0700

    Modified data schemas.

--- models/users/users-schema.js
+++ models/users/users-schema.js
@@ -6,6 +6,17 @@ const usersSchema = mongoose.Schema(
         password: String,
         firstName: String,
         lastName: String,
+        address: String,
+        affiliation: String,
+        email: String,
+        phone: String,
+        type: {type: String, enum: ['JOB SEEKER', 'RECRUITER']},
+        education: String,
+        experience: String,
+        skills: String,
+        licenceAndCertifications: String,
+        users: [{type: String, ref: 'UserModel'}],
+        jobs:[{type: String, ref: 'JobModel'}],
     }, {collection: 'users'});
 
 module.exports = usersSchema;
