ba6a40bc6ea94878fd149b829441753b798aaecb
Rijkstarhn <32129291+Rijkstarhn@users.noreply.github.com>
2021-04-22 02:13:19 -0700

    Completed add a job by user function.

--- daos/jobs-dao.js
+++ daos/jobs-dao.js
@@ -2,16 +2,26 @@ const jobsModel = require('../models/jobs/jobs-model');
 
 const findAllJobs = () => jobsModel.find();
 
+const findAJob = (job) => jobsModel.find(job);
+
 const findJobById = (jid) => jobsModel.findById(jid);
 
 const findJobsForUser = (uid) => jobsModel.find({userId: uid});
 
-const createJob = (job) => jobsModel.create(job);
+const createJob = (job) => {
+    return jobsModel.create(job);
+}
 
 const deleteJob = (jid) => jobsModel.remove({_id: jid});
 
 const updateJob = (jid, job) => jobsModel.updateOne({_id: jid},
     {$set: job});
+
+const updateJobUser = (jid, uid) => jobsModel.updateOne(
+    {_id: jid},
+    {$push: {userId: uid}});
+
+const countJobs = () => jobsModel.countDocuments();
 
 module.exports = {
     findAllJobs: findAllJobs,
@@ -20,4 +30,7 @@ module.exports = {
     createJob: createJob,
     deleteJob: deleteJob,
     updateJob: updateJob,
+    findAJob,
+    updateJobUser,
+    countJobs,
 };
--- models/jobs/jobs-schema.js
+++ models/jobs/jobs-schema.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose');
 
 const jobSchema = mongoose.Schema({
     jobId: String,
-    userId: String,
     userId: {type: [String], ref: 'UserModel'},
     title: String,
+    company: String,
+    createdTime: String,
+    note: String,
 }, {collection: 'jobs'});
 
 module.exports = jobSchema;
